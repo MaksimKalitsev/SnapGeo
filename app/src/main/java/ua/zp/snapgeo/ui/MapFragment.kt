d142da55d1d7a7c93c1cef17207f7cd8a5b57df6
Maksim.Kalitsev <maksim.kalitsev@ukr.net>
2023-06-08 11:39:36 +0300

    add current location on map

--- app/src/main/java/ua/zp/snapgeo/ui/MapFragment.kt
+++ app/src/main/java/ua/zp/snapgeo/ui/MapFragment.kt
@@ -1,37 +1,49 @@
 package ua.zp.snapgeo.ui
 
 import android.Manifest
-import android.content.Intent
+import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.provider.MediaStore
-import androidx.fragment.app.Fragment
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import ua.zp.snapgeo.R
 import ua.zp.snapgeo.databinding.FragmentMapBinding
 
-class MapFragment : Fragment() {
+open class MapFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var viewModel: MapViewModel
+
+    val MY_PERMISSIONS_REQUEST_LOCATION = 99
+
+    var mLocationRequest: LocationRequest? = null
+    var mLastLocation: Location? = null
+
+
+    var mFusedLocationClient: FusedLocationProviderClient? = null
+
+    private lateinit var mGoogleMap: GoogleMap
 
 
     override fun onCreateView(
@@ -45,6 +57,112 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+
+        mLocationRequest =
+            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 12000).apply {
+                setWaitForAccurateLocation(true)
+                setMaxUpdateAgeMillis(100000)
+            }.build()
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            //Location Permission already granted
+            mFusedLocationClient!!.requestLocationUpdates(
+                mLocationRequest!!, mLocationCallback, Looper.myLooper()
+            )
+            mGoogleMap.isMyLocationEnabled = true
+        } else {
+            //Request Location Permission
+            checkLocationPermission()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        //stop location updates when Activity is no longer active
+        mFusedLocationClient?.removeLocationUpdates(mLocationCallback)
+    }
+
+    private var mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList = locationResult.locations
+            if (locationList.size > 0) {
+                //The last location in the list is the newest
+                val location = locationList[locationList.size - 1]
+                mLastLocation = location
+                val latLng = LatLng(location.latitude, location.longitude)
+                // Creating a marker
+                val markerOptions = MarkerOptions()
+
+                // Setting the position for the marker
+                markerOptions.position(latLng)
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(location.latitude.toString() + " : " + location.longitude)
+
+                // Clears the previously touched position
+                mGoogleMap.clear()
+
+                // Animating to the touched position
+                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+
+                // Placing a marker on the touched position
+                mGoogleMap.addMarker(markerOptions)
+                val cameraPosition =
+                    CameraPosition.Builder().target(LatLng(latLng.latitude, latLng.longitude))
+                        .zoom(18f).build()
+                mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+            }
+        }
+    }
+
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                AlertDialog.Builder(requireContext()).setTitle("Location Permission Needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton(
+                        "OK"
+                    ) { _, i -> //Prompt the user once explanation has been shown
+                        ActivityCompat.requestPermissions(
+                            requireActivity(),
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            MY_PERMISSIONS_REQUEST_LOCATION
+                        )
+                    }.create().show()
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    MY_PERMISSIONS_REQUEST_LOCATION
+                )
+            }
+        }
     }
 }
